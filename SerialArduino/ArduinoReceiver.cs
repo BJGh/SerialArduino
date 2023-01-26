e07bfb4943dbfc91811fe056be60a9f95ff75b67
Burin-Zhargal <papaev.b.n@gmail.com>
2023-01-26 11:34:44 +0300

    Добавьте файлы проекта.

---
+++ SerialArduino/ArduinoReceiver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SerialArduino
+{
+
+    class ArduinoReceiver
+    {
+
+        static void Main(string[] args)
+        {
+            SerialPort sp = new SerialPort("COM5", 9600);
+            Console.WriteLine("Hello World!");
+            sp.Open();
+            while(true)
+            {
+                string data_r = sp.ReadLine();
+                Console.WriteLine(data_r);
+            }
+        }
+    }
+}
